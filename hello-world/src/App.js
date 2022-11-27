6f31d088425516a3ebd09245509d62d6a7467c68
Vivek Thakur <vivekthakurcse779@gmail.com>
2022-11-27 14:09:09 +0530

    Hello-World

---
+++ hello-world/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello ,World!</p>
+        <p>Lets React with React</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
